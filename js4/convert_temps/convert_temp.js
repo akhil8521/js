af91a3679383d0b9d4d79efdcf3fb1d947f6b3f7
akhil jahagirdar <akhil@dwi.agency>
2021-07-18 20:09:02 +0200

    commit01 updated js 3,4

--- js4/convert_temps/convert_temp.js
+++ js4/convert_temps/convert_temp.js
@@ -1,101 +1,48 @@
 "use strict";
-var $ = function (id) { return document.getElementById(id); };
+var $ = function (id) {
+  return document.getElementById(id);
+};
 
+var toCelsius = function () {
+  $("degree_label_1").innerHTML = "Enter F degrees:";
+  $("degree_label_2").innerHTML = "Degrees Celsius:";
+  clearTextBoxes();
+};
+
+var toFahrenheit = function () {
+  $("degree_label_1").innerHTML = "Enter C degrees:";
+  $("degree_label_2").innerHTML = "Degrees Fahrenheit:";
+  clearTextBoxes();
+};
 
 var clearTextBoxes = function () {
-    $("degrees_entered").value = "";
-    $("degrees_computed").value = "";
+  $("degrees_entered").value = "";
+  $("degrees_computed").value = "";
+  $("degrees_entered").focus();
+};
+
+var convertTemp = function () {
+  var degrees = parseFloat($("degrees_entered").value);
+  var degresStr = $("degrees_entered").value;
+
+  if (isNaN(degresStr)) {
+    alert("You must enter a valid number for degrees.");
+    $("degrees_entered").focus();
+    $("degrees_entered").select();
+  } else {
+    if ($("to_celsius").checked) {
+      var celsius = (degrees - 32) * (5 / 9);     
+      $("degrees_computed").value = celsius.toFixed(0);
+    } else {
+      var fahrenheit = (9 / 5) * degrees + 32;  
+      $("degrees_computed").value = fahrenheit.toFixed(0);
+    }
+  }
 };
 
 window.onload = function () {
-    $("convert").onclick = convertTemp;
-    $("to_celsius").onclick = toCelsius;
-    $("to_fahrenheit").onclick = toFahrenheit;
-    $("degrees_entered").focus();
-};
-
-/**
-
-* convert temprature
-
-* fahrenheit to celsius or celsius to fahrenheit
-
-* @param e event object
-
-*/
-
-var convertTemp = function(e){
-
-    var enteredVal = $("degrees_entered").value;
-
-    if (isNaN(enteredVal)){
-
-        alert("You must enter a valid number for degrees.");
-
-        return;
-
-    }
-
-    if ($("to_celsius").checked){           // condition for F to C conversion
-
-        var C = (enteredVal - 32) * 5 / 9;           // calculate celsius from fahrenheit
-
-        $("degrees_computed").value = C;    // update text box
-
-    }else if ($("to_fahrenheit").checked){  // condition for C to F conversion
-
-        var F = (9 * enteredVal / 5) + 32;           // calculate fahrenheit from celsius
-
-        $("degrees_computed").value = F;    // update text box
-
-    }
-
-}
-
-/**
-
-* change labled and conversion
-
-* @param e
-
-*/
-
-var toCelsius = function (e) {
-
-    $("degree_label_1").innerHTML = "Enter F degrees:";
-
-    $("degree_label_2").innerHTML = "Degrees Celsius:";
-
-    clearTextBoxes();
-
-}
-
-/**
-
-* change labled and conversion
-
-* @param e
-
-*/
-
-var toFahrenheit = function (e) {
-
-    $("degree_label_1").innerHTML = "Enter C degrees:";
-
-    $("degree_label_2").innerHTML = "Degrees Fahrenheit:";
-
-    clearTextBoxes();
-
-}
-
-window.onload = function() {
-
-    $("convert").onclick = convertTemp;
-
-    $("to_celsius").onclick = toCelsius;
-
-    $("to_fahrenheit").onclick = toFahrenheit;
-
-    $("degrees_entered").focus();
-
+  $("convert").onclick = convertTemp;
+  $("to_celsius").onclick = toCelsius;
+  $("to_fahrenheit").onclick = toFahrenheit;
+  $("degrees_entered").focus();
 };
--- js4/faqs/faqs.js
+++ js4/faqs/faqs.js
@@ -1,35 +1,47 @@
 "use strict";
-var $ = function(id) { return document.getElementById(id); };
-
-// the event handler for the click event of each h2 element
-var toggle = function() {
-    var h2 = this;                    // clicked h2 tag     
-    var div = h2.nextElementSibling;  // h2 tag's sibling div tag
-
-    // toggle plus and minus image in h2 elements by adding or removing a class
-    if (h2.hasAttribute("class")) { 
-        h2.removeAttribute("class");	
-    } else { 
-        h2.setAttribute("class", "minus"); 
-    }
-
-    // toggle div visibility by adding or removing a class
-    if (div.hasAttribute("class")) { 
-        div.removeAttribute("class");
-    } else { 
-        div.setAttribute("class", "open"); 
-    } 
+var $ = function (id) {
+  return document.getElementById(id);
 };
 
-window.onload = function() {
-    // get the h2 tags
-    var faqs = $("faqs");
-    var h2Elements = faqs.getElementsByTagName("h2");
-    
-    // attach event handler for each h2 tag	    
-    for (var i = 0; i < h2Elements.length; i++ ) {
-    	h2Elements[i].onclick = toggle;
+// the event handler for the click event of each h2 element
+var toggle = function () {
+  var h2 = this; // clicked h2 tag
+  var div = h2.nextElementSibling; // h2 tag's sibling div tag
+
+  // toggle plus and minus image in h2 elements by adding or removing a class
+  if (h2.hasAttribute("class")) {
+    h2.removeAttribute("class");
+  } else {
+    h2.setAttribute("class", "minus");
+  }
+
+  // toggle div visibility by adding or removing a class
+  if (div.hasAttribute("class")) {
+    div.removeAttribute("class");
+  } else {
+    div.setAttribute("class", "open");
+  }
+
+  const h2Elements = document.getElementsByTagName("h2"); //get all h2 elements in an array
+
+  for (let h2Element of h2Elements) {
+    if (h2Element != this) {
+      //make the non-clicked h2's show plus sign and hide corresponding divs by removing their class attributes if any
+      h2Element.removeAttribute("class");
+      h2Element.nextElementSibling.removeAttribute("class");
     }
-    // set focus on first h2 tag's <a> tag
-    h2Elements[0].firstChild.focus();       
+  }
+};
+
+window.onload = function () {
+  // get the h2 tags
+  var faqs = $("faqs");
+  var h2Elements = faqs.getElementsByTagName("h2");
+
+  // attach event handler for each h2 tag
+  for (var i = 0; i < h2Elements.length; i++) {
+    h2Elements[i].onclick = toggle;
+  }
+  // set focus on first h2 tag's <a> tag
+  h2Elements[0].firstChild.focus();
 };
